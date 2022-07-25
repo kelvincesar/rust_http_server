d387b3dbb9a45c60746030e48a6e5f26322e3b06
Kelvin C. Andrade <kelvincandrade@gmail.com>
2022-07-25 10:41:10 -0300

    Finalizado parte 01

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-
+        //println!("handling connection!");
         let stream = stream.unwrap();
         handle_connection(stream);
     }
@@ -19,36 +19,51 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!("Conexão estabilizada!");
     
     // Closer look to an http request:
     //Method Request-URI HTTP-Version CRLF
     //headers CRLF
     //message-body
     
-
+    println!("Conexão estabilizada!");
     println!(
         "- Request: {}",
         String::from_utf8_lossy(&buffer[..]) // buffer inteiro
     );
 
     
-    let status_line = "HTTP/1.1 200 OK";
+    let get = b"GET / HTTP/1.1\r\n"; // byte array
 
-    let contents = fs::read_to_string("index.html").unwrap();
-    // response: HTTP/1.1 200 OK\r\n\r\n
+    // Direcionamento 
+    let (status_line, file_name) = 
+        if buffer.starts_with(get){
+            ("HTTP/1.1 200 OK", "index.html")
+
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    // response: HTTP/1.let status_line = "HTTP/1.1 200 OK";let status_line = "HTTP/1.1 200 OK";1 200 OK\r\n\r\n
+   
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
         contents.len(),
         contents
     );
-    let status_line = "HTTP/1.1 200 OK";
-
-    // evnia resposta
+    // envia  resposta
     stream.write(response.as_bytes()).unwrap();
     // will wait and prevent the program from continuing until 
     //jall the bytes are written to the connection
-    stream.flush().unwrap(); 
+    stream.flush().unwrap();     
+
+
+    
+
+
+    
+
+
 
 }
